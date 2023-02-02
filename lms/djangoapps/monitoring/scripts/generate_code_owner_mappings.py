81c0858712d9aabcf4a05d660028e9b7fd20c4ff
Xavier Perez <xavier.perez@sixninesit.com>
2023-02-02 13:59:49 -0300

    Configured django-wiki to 1.1.4 release

--- lms/djangoapps/monitoring/scripts/generate_code_owner_mappings.py
+++ lms/djangoapps/monitoring/scripts/generate_code_owner_mappings.py
@@ -40,7 +40,7 @@ EDX_REPO_APPS = {
     'organizations': 'https://github.com/openedx/edx-organizations',
     'search': 'https://github.com/openedx/edx-search',
     'super_csv': 'https://github.com/openedx/super-csv',
-    'wiki': 'https://github.com/openedx/django-wiki',
+    'wiki': 'https://github.com/openedx/django-wiki?ref=v1.1.4',
 }
 
 # Maps edx-platform installed Django apps to the third-party repo that contains
